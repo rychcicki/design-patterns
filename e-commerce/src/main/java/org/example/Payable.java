883239d7f796ac88747a3840b0f14771e5c253cf
Marcin <phppoznan@gmail.com>
2023-09-06 21:28:56 +0200

    Revert "feat: - added payment's methods"
    This reverts commit 6836a4c090e040e2fdd3690b7155e3a65e9776c4.

--- e-commerce/src/main/java/org/example/Payable.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.math.BigDecimal;
-
-public interface Payable<T> {
-    BigDecimal USD_TO_BTC = new BigDecimal("0.0000384");
-
-    void pay(BigDecimal amount);
-}
